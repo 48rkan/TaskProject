a6885f7a2c12baab858075a9d37969fbc1a33ade
48rkan <erkanemiraslan@gmail.com>
2023-08-11 11:51:57 +0400

    bug fix

--- TaskProject/Utils/Extensions.swift
+++ TaskProject/Utils/Extensions.swift
@@ -7,20 +7,20 @@
 //
 
 import UIKit
-import JGProgressHUD
+//import JGProgressHUD
 
 extension UIViewController {
-    static let hud = JGProgressHUD(style: .dark)
-
-    func showLoader(_ show: Bool) {
-        view.endEditing(true)
-
-        if show {
-            UIViewController.hud.show(in: view)
-        } else {
-            UIViewController.hud.dismiss()
-        }
-    }
+//    static let hud = JGProgressHUD(style: .dark)
+//
+//    func showLoader(_ show: Bool) {
+//        view.endEditing(true)
+//
+//        if show {
+//            UIViewController.hud.show(in: view)
+//        } else {
+//            UIViewController.hud.dismiss()
+//        }
+//    }
     
     func showMessage(withTitle title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
